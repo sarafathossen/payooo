8ea0227d223f42e305433069a769fdf45d791ce7
sarafat <sarafathossen01@gmail.com>
2025-08-24 19:47:01 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// console.log('Joining Test')
+
+
+document.getElementById('loginButton').addEventListener('click', function(e){
+    e.preventDefault()
+    const mobileNumber = 012345678910
+    const pinNumber = 1234
+
+    const mobileNumberValue = parseInt(document.getElementById('mobile-number').value)
+    const pinNumberValue = parseInt(document.getElementById('pin-number').value)
+    console.log(mobileNumberValue, pinNumberValue)
+    if(mobileNumberValue === mobileNumber && pinNumberValue === pinNumber){
+        window.location.href="./home.html"
+    }
+    else{
+        alert('Invalid Phone Number Or Pin Number')
+    }
+})
